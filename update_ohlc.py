a082e4dbf22a43d70d92452348840152cbcf65bf
Ross Coates <ross_coates82@hotmail.com>
2023-02-08 12:37:52 +0000

    changed update_ohlc to download 5min data instead of 15min

--- update_ohlc.py
+++ update_ohlc.py
@@ -29,7 +29,7 @@ start = time.perf_counter()
 pairs = funcs.get_pairs()
 
 
-def iterations(pair, tf):
+def iterations(n, pair, tf):
     folder = f"bin_ohlc_{tf}"
     if live:
         ohlc_data = Path(f'/media/coding/') / folder
@@ -47,26 +47,28 @@ def iterations(pair, tf):
         df = funcs.get_ohlc(pair, tf, '2 years ago UTC')
         df_end = time.perf_counter()
         elapsed = df_end - df_start
-        print(f'downloaded {pair} from scratch, took {elapsed % 60:.1f}s')
+        print(f'downloaded {pair} from scratch, took {int(elapsed // 60)}m {elapsed % 60:.1f}s')
 
     # print(pair, df.timestamp.iloc[-1])
 
     max_dict = {'1m': 1051200,
+                '5m': 210240,
                 '15m': 70080,
                 '1h': 17520}
     max_len = max_dict[tf]  # returns 2 years worth of timeframe periods
     if len(df) > max_len:
         df = df.tail(max_len).reset_index(drop=True)
     df.to_pickle(filepath)
-    # print(f"{pair} ohlc length: {len(df)}")
+    print(f"{n} {pair} ohlc length: {len(df)}")
 
 
-iterations('BTCUSDT', '1m')
-iterations('ETHUSDT', '1m')
+iterations(0, 'BTCUSDT', '1m')
+iterations(1, 'ETHUSDT', '1m')
 
-for pair in pairs:
-    iterations(pair, '15m')
-    iterations(pair, '1h')
+for n, pair in enumerate(pairs):
+    iterations(n, pair, '5m')
+    # iterations(n, pair, '15m')
+    # iterations(n, pair, '1h')
 
 end = time.perf_counter()
 all_time = end - start
