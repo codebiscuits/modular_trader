e8d063fef943b63cef5e4039dcde334a4f3320eb
Ross Coates <ross_coates82@hotmail.com>
2023-02-20 10:52:04 +0000

    update_ohlc now downloads 1m data for btcusdt and eth usdt and 5m data for everything. all data is saved as parquet files

--- update_ohlc.py
+++ update_ohlc.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from pandas import errors
 import keys
 import time
 from datetime import datetime
@@ -7,12 +6,10 @@ import binance_funcs as funcs
 from binance.client import Client
 from pushbullet import Pushbullet
 from pathlib import Path
-from config import not_pairs
+from sessions import LightSession
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.expand_frame_repr', False)
-
-# TODO after migrating to polars, i want to incorporate market cap data from coingecko/coinmarketcap
 
 client = Client(keys.bPkey, keys.bSkey)
 
@@ -20,32 +17,53 @@ pb = Pushbullet('o.H4ZkitbaJgqx9vxo5kL2MMwnlANcloxT')
 
 now = datetime.now().strftime('%d/%m/%y %H:%M')
 
-# check to see if this is running on the raspberry pi or not
-pi2path = Path('/home/ubuntu/rpi_2.txt')
-if live := pi2path.exists():
+session = LightSession()
+
+if session.live:
     print('-:-' * 10, f' {now} running update_ohlc ', '-:-' * 10)
 else:
     print('*** Warning: Not Live ***')
 
+
+# def get_filepath(pair, tf):
+#     folder = f"bin_ohlc_{tf}"
+#     if live:
+#         ohlc_data = Path('/media/coding/') / folder
+#     else:
+#         ohlc_data = Path('/home/ross/Documents/backtester_2021/') / folder
+#     ohlc_data.mkdir(exist_ok=True)
+#     return Path(f'{ohlc_data}/{pair}.parquet')
+
+
 start = time.perf_counter()
 
-pairs = funcs.get_pairs()
+pairs = list(session.pairs_data.keys())
+
+# TODO i want to try running a function which records the roc of different timeframes for each pair in a separate file
+#  for setup_scanner to use as a ranking metric (relative strength)
+
+# TODO once i have a market cap column, i can make a 24h volume / market cap column
+
+# TODO i also want really high timeframe stuff like 200 week sma and emas. it might be possible to download daily or
+#  weekly ohlc data from exchange to get that, or i might be able to get those kind of metrics from coingecko. these
+#  will need to be interpolated too
+
+# TODO it would also be nice to have some on-chain metrics being included in this script as well but i will have to work
+#  out where to get that data from
 
 
 def iterations(n, pair, tf):
-    folder = f"bin_ohlc_{tf}"
-    if live:
-        ohlc_data = Path('/media/coding/') / folder
-    else:
-        ohlc_data = Path('/home/ross/Documents/backtester_2021/') / folder
-    ohlc_data.mkdir(exist_ok=True)
-    filepath = Path(f'{ohlc_data}/{pair}.pkl')
+    # print(f"{n} {pair} {tf}")
+    session.set_ohlc_tf(tf)
+    # print(session.ohlc_data)
+    filepath = Path(f'{session.ohlc_data}/{pair}.parquet')
+    # print(filepath)
+    #-------------------- if theres already some local data -------------------------#
     if filepath.exists():
-        print(filepath)
-        df = pd.read_pickle(filepath)
+        df = pd.read_parquet(filepath)
         if len(df) > 2:
             df = funcs.update_ohlc(pair, tf, df)
-
+    # -------------------- if theres no local data yet -------------------------#
     else:
         df_start = time.perf_counter()
         df = funcs.get_ohlc(pair, tf, '2 years ago UTC')
@@ -55,27 +73,22 @@ def iterations(n, pair, tf):
 
     # print(pair, df.timestamp.iloc[-1])
 
-    max_dict = {'1m': 1051200,
-                '5m': 210240,
-                '15m': 70080,
-                '1h': 17520}
+    max_dict = {'1m': 1051200, '5m': 210240, '15m': 70080, '1h': 17520}
     max_len = max_dict[tf]  # returns 2 years worth of timeframe periods
     if len(df) > max_len:
         df = df.tail(max_len).reset_index(drop=True)
-    df.to_pickle(filepath)
-    print(f"{n} {pair} ohlc length: {len(df)}")
+    df.to_parquet(filepath)
+    # print(f"{n} {pair} ohlc length: {len(df)}")
 
 
 iterations(0, 'BTCUSDT', '1m')
 iterations(1, 'ETHUSDT', '1m')
 
 for n, pair in enumerate(pairs):
-    iterations(n, pair, '15m')
     try:
         iterations(n, pair, '5m')
     except:
         continue
-    # iterations(n, pair, '1h')
 
 end = time.perf_counter()
 all_time = end - start
