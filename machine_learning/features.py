1b3b1a27c5f505db5d0acf395c97d65045bd24e6
codebiscuits <ross_coates82@hotmail.com>
2023-05-19 18:37:46 +0100

    added some features, need to find a way to balance the labels and i'm going to switch to predicting binary profit / loss labels instead of continuous R values

--- machine_learning/features.py
+++ machine_learning/features.py
@@ -1,5 +1,6 @@
 import indicators as ind
 import pandas as pd
+import numpy as np
 
 def stoch_vwma_ratio(df, lookback: int) -> pd.Series:
     return ind.stochastic(df.close / df.vwma, lookback)
@@ -37,6 +38,24 @@ def bull_bear_bar(df) -> pd.DataFrame:
     df = ind.bull_bear_bar(df)
     df['bullish_bar'] = df.bullish_bar.shift(1)
     df['bearish_bar'] = df.bearish_bar.shift(1)
+
+    return df
+
+
+def htf_fractals_proximity(df: pd.DataFrame, orig_tf: str, htf: str='W', frac_width: int=5) -> pd.DataFrame:
+    """resamples to weekly or monthly timeframe, calculates williams fractals on that, works out which is closest to the
+    current price and returns the pct difference"""
+
+    closes = df.close.resample(htf).shift(1) # make sure the close price for each week is recorded on the following week
+
+    # only need to resample close column
+    fractal_high = np.where(closes == closes.rolling(frac_width, center=True).max(), closes, np.nan)
+    fractal_low = np.where(closes == closes.rolling(frac_width, center=True).min(), closes, np.nan)
+
+    # make a list of all the levels with their timestamps (they are only valid levels after they have been established)
+
+    # for each period in the original df, record which is the closest VALID level, then create a column that represents
+    # how far those prices are from the open price or vwma
 
     return df
 
