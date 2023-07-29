2b8977266aad3a5fbbcd2236307da1b95df6c7c8
Ross Coates <ross_coates82@hotmail.com>
2023-07-29 23:14:46 +0100

    updated inspect_open_trades

--- inspect_records/inspect_open_trades.py
+++ inspect_records/inspect_open_trades.py
@@ -5,55 +5,70 @@ from pushbullet import Pushbullet
 from pathlib import Path
 import json
 import trade_records_funcs as trf
+from binance.client import Client
+import resources.keys as keys
+from datetime import datetime, timezone
 
 pb = Pushbullet('o.H4ZkitbaJgqx9vxo5kL2MMwnlANcloxT')
 pd.set_option('display.max_rows', None)
 pd.set_option('display.expand_frame_repr', False)
 pd.set_option('display.precision', 4)
 
+client = Client(keys.bPkey, keys.bSkey)
+
 records_folder_1 = Path('/home/ross/coding/pi_down/modular_trader/records')
 records_folder_2 = Path('/home/ross/coding/pi_2/modular_trader/records')
+
+now = datetime.now().timestamp()
 
 def print_positions(data, name):
     print(f'\n{name} open positions:')
     if data:
         for k, v in data.items():
             print(f"\n{'-'*20}\n{k}")
-            long_rpnls = []
-            short_rpnls = []
             for a, b in v.items():
-                a = float(a) / 1000 if float(a) > 20_000_000_000 else float(a)
-                a = int(a) * 1_000_000_000
-                if b['trade'][0].get('wanted', True):
-                    # print(f"\n{a}")
-                    # pprint(b)
-                    state = b['trade'][0]['state']
-                    dir = b['trade'][0]['direction']
-                    rpnl = 0
-                    for t in b['trade']:
-                        if t.get('rpnl'):
-                            rpnl += t['rpnl']
-                    if dir == 'spot':
-                        long_rpnls.append((a, state, rpnl))
-                    elif dir == 'long':
-                        long_rpnls.append((a, state, rpnl))
-                    elif dir == 'short':
-                        short_rpnls.append((a, state, rpnl))
-            for k, v in {'long': long_rpnls, 'short': short_rpnls}.items():
-                rpnl_df = pd.DataFrame(v, columns=['timestamp', 'state', 'rpnl'])
-                rpnl_df = rpnl_df.sort_values('timestamp').reset_index(drop=True)
+                price = float(client.get_symbol_ticker(symbol=a)['price'])
+                direction = b['position']['direction']
+                duration = (now - (b['position']['open_time'] / 1000))
+                duration_str = f"{int(duration // 3600)}h {int((duration / 60) % 60)}m"
+                current_value = price * float(b['position']['base_size'])
+                liability = float(b['position']['liability'])
+                current_stop = float(b['position']['hard_stop'])
+                open_risk_pct = (((price - current_stop) / price)
+                                 if direction == 'long'
+                                 else ((current_stop - price) / price))
+                open_risk_usdt = current_value * open_risk_pct
 
-                rpnl_df['timestamp'] = pd.to_datetime(rpnl_df.timestamp, utc=True)
-                rpnl_df['ema_4'] = rpnl_df.rpnl.ewm(4).mean()
-                rpnl_df['ema_8'] = rpnl_df.rpnl.ewm(8).mean()
-                rpnl_df['ema_16'] = rpnl_df.rpnl.ewm(16).mean()
-                rpnl_df['ema_32'] = rpnl_df.rpnl.ewm(32).mean()
-                rpnl_df['ema_64'] = rpnl_df.rpnl.ewm(64).mean()
-                print('\n', k)
-                print(rpnl_df.tail(10))
+                # check if it's a new trade (no adds or tps)
+                new = (b['position']['base_size'] == b['position']['init_base_size']) and (len(b['trade']) == 1)
+
+                if new:
+                    pnl_ratio = ((price / float(b['trade'][0]['exe_price']))
+                                 if direction == 'long' else
+                                 (float(b['trade'][0]['exe_price']) / price))
+                else:
+                    costs = 0.0
+                    returns = current_value
+                    for i in b['trade']:
+                        if i['action'] in ['open', 'add']:
+                            costs += float(i['quote_size'])
+                        elif i['action'] == 'tp':
+                            returns += float(i['quote_size'])
+
+                    pnl_ratio = (returns / costs) if direction == 'long' else (costs / returns)
+
+                pnl_pct = f"{(pnl_ratio - 1):.1%}"
+                pnl_usdt = float(b['trade'][0]['quote_size']) * (pnl_ratio - 1)
+
+                print('')
+                print(f"{a} {direction}, duration: {duration_str}")
+                print(f"Value: {current_value:.2f} USDT, liability: {liability:.2f} USDT")
+                print(f"PnL: {pnl_pct}, {pnl_usdt:.2f} USDT")
+                print(f"Open risk: {open_risk_pct:.1%}, {open_risk_usdt:.2f} USDT")
     else:
         print('\nNone')
 
+    # pprint(b)
+
 data = trf.load_all(records_folder_2, ['open'])
-pprint(data)
-# print_positions(data, 'pi 2')
+print_positions(data, 'pi 2')
