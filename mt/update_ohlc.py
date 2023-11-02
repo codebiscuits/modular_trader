44f6ee6f96bf6ac4e1f66a99c30225fc7bec1c81
codebiscuits <ross_coates82@hotmail.com>
2023-11-02 14:21:04 +0000

    changed the length of ohlc history that update_ohlc.py will download from scratch. now 1m timeframe will download 2 years' worth, and all other timeframes will download 6 years. also removed the code block which trims the length of the ohlc before saving it

--- mt/update_ohlc.py
+++ mt/update_ohlc.py
@@ -41,8 +41,9 @@ volatilities_1w = {}
 lengths = {}
 
 def from_scratch(session, pair, tf):
+    y = 2 if tf == '1m' else 6
     df_start = time.perf_counter()
-    df = funcs.get_ohlc(pair, tf, '2 years ago UTC', session)
+    df = funcs.get_ohlc(pair, tf, f'{y} years ago UTC', session)
     df_end = time.perf_counter()
     elapsed = df_end - df_start
     logger.info(f'downloaded {pair} from scratch, took {int(elapsed // 60)}m {elapsed % 60:.1f}s')
@@ -79,11 +80,11 @@ def iterations(n, session, pair, tf):
         df = from_scratch(session, pair, tf)
         # print(f'{n} downloaded {pair} from scratch')
 
-    max_dict = {'1m': 1051200, '5m': 210240, '1h': 17520}
-    max_len = max_dict[tf]  # returns 2 years worth of timeframe periods
-    if len(df) > max_len:
-        # print(f"trimming ohlc from {len(df)} to {max_len}")
-        df = df.tail(max_len).reset_index(drop=True)
+    # max_dict = {'1m': 1051200, '5m': 210240, '1h': 17520}
+    # max_len = max_dict[tf]  # returns 2 years worth of timeframe periods
+    # if len(df) > max_len:
+    #     # print(f"trimming ohlc from {len(df)} to {max_len}")
+    #     df = df.tail(max_len).reset_index(drop=True)
 
     df['timestamp'] = pd.to_datetime(df['timestamp'])
 
